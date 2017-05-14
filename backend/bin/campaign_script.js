4961a6d99d516c18438df62646df4fac637fc088
Xantomen <xantomen@gmail.com>
2017-05-14 16:31:14 +0200

    Proper ManyToMany relationships from Parliamentarian and Constituency in relation to Campaign. Added "times_requested" to Campaign model.

---
+++ backend/bin/campaign_script.js
@@ -0,0 +1,54 @@
+// Copyright IBM Corp. 2015,2016. All Rights Reserved.
+// Node module: loopback-example-database
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+var path = require('path');
+
+var app = require(path.resolve(__dirname, '../server/server'));
+var ds = app.datasources.considr_db;
+ds.automigrate('Campaign', function(err) {
+  if (err) throw err;
+
+  var campaigns = 
+  [
+	  {
+		"draft": true,
+		"active": false,
+		"last_updated": "2017-05-13T11:48:03.158Z",
+		"creation_date": "2017-05-13T11:48:03.158Z",
+		"campaign_picture_path": "http:",
+		"campaign_code": "001",
+		"creator_campaign": "Test1",
+		"title": "First Campaign",
+		"description": "This is the first campaign's description",
+		"reason": "This is the reason why",
+		"script": "This is the script"
+	  },
+	  {
+		"draft": true,
+		"active": false,
+		"last_updated": "2017-05-13T11:48:03.158Z",
+		"creation_date": "2017-05-13T11:48:03.158Z",
+		"campaign_picture_path": "http:",
+		"campaign_code": "002",
+		"creator_campaign": "Test2",
+		"title": "Second Campaign",
+		"description": "This is the second campaign's description",
+		"reason": "This is the reason why",
+		"script": "This is the script"
+	  }
+	];
+  var count = campaigns.length;
+  campaigns.forEach(function(campaign) {
+    app.models.Campaign.create(campaign, function(err, model) {
+      if (err) throw err;
+
+      console.log('Created:', model);
+
+      count--;
+      if (count === 0)
+        ds.disconnect();
+    });
+  });
+});
--- backend/common/models/campaign.js
+++ backend/common/models/campaign.js
@@ -1,5 +1,22 @@
 'use strict';
 
 module.exports = function(Campaign) {
-
+	Campaign.getCampaignsWithConstituency = function(fk,cb) {
+        var response = "Yay! Hero did a good thing";
+				
+		Campaign.find({include: ['constituency_list'], limit: 3}, function(err, campaigns) 
+		{ /* ... */ 						
+			cb(null, campaigns);
+		});					
+				
+    };
+    Campaign.remoteMethod('getCampaignsWithConstituency', {
+		accepts: {arg: 'fk', type: 'string'},
+        http: { path: '/getCampaignsWithConstituency', verb: 'get' },
+        returns: { arg: 'result', type: 'object' }
+    }); 
 };
+
+//Campaign.find({where: {"campaign_code": "002"}, limit: 3}, function(err, campaigns) 
+//Campaign.find({where: {constituency_list: [{constituency_code: '002'}]}, limit: 3}, function(err, campaigns) 
+

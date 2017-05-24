956e438a2857cb5e43899b21e91e6ca33cde6f69
Xantomen <xantomen@gmail.com>
2017-05-24 20:57:54 +0200

    CORS Test

--- backend/server/server.js
+++ backend/server/server.js
@@ -5,6 +5,12 @@ var boot = require('loopback-boot');
 
 var app = module.exports = loopback();
 
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 app.start = function() {
   // start the web server
   return app.listen(function() {
